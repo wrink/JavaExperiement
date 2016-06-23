9a874f5a3e32c0a0d30d6d8d413579a871e4b273
David Wikler <dwikler@dwikler-nbk1.ip-soft.net>
2016-06-23 14:53:51 -0400

    test

---
+++ Tester.java
@@ -0,0 +1,9 @@
+public class Tester {
+   public Tester () {
+      System.out.println ("Hello there");
+   }
+
+   public static void main(String argv[]) {
+     System.out.println("Network Class Loaders");
+   }
+}
